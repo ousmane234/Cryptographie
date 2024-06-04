478ddc90d2319d54d1fd8a600b400367abdace8c
ousmane234 <lousmane2013@gmail.com>
2024-06-04 12:08:49 +0000

    modification 1

--- rsa/from-scratch/client.py
+++ rsa/from-scratch/client.py
@@ -2,3 +2,35 @@
 # Après reception, il génère une clef secrète S aléatoire < n
 # chiffre la C=S^e mod n  
 # envoie C au serveur et l'affiche sur la console.
+import random   
+import requests # type: ignore
+url = "http://127.0.0.1:5001"
+
+# demande de la paire de cle publique :
+response = requests.get(f'{url}/pub_key_exchange')
+if response.status_code == 200 :
+	# extraire les données de la requetes :
+	response_data =  response.json()
+	print(response_data)
+	pub_key_server =response_data["public_key"] # clé public sous forme de dictionnaire
+	e = pub_key_server["e"] 
+	n = pub_key_server["n"]
+	# generation d'une cle secrete S aleatoire < n 
+	secret_key = random.randint(1 ,n)
+	# chiffrement de la cle secrete avec la cle public du server 
+	secret_key_ciphered = pow(secret_key , e , n)
+	data = {"secret_key_ciphered": secret_key_ciphered}
+	print(data)
+	# envoyer le secret au server
+	response =requests.post(f'{url}/secret_exchange', json = data)
+	if response.status_code ==200:
+		# afficher le secret sur  la console
+		print(f'cle secret : {secret_key_ciphered}')
+	else:
+		print("Erreur de reception du secret")	
+	
+
+else:
+	print("Erreur de reception de la cle pub du server :", response.text)
+
+
--- rsa/from-scratch/serveur.py
+++ rsa/from-scratch/serveur.py
@@ -2,3 +2,35 @@
 # s'il reçoit la demande du client il lui envoie  pub=(e,n)
 # s'il recoit le chiffré de la clé générée par le client, alors il calcul la clé partagé
 # et l'affiche sur la console.
+import flask 
+import random 
+app = flask.Flask("__name__")
+# generation de la paire de clé publique et privé
+e , p ,q = 7, 11 ,13 
+n  , phi_n = p*q  , (p-1)*(q-1)
+pub_key_server = { 'e':e , 'n': n}
+d = pow( e , -1 , phi_n)
+private_key = {"d": d, "n": n}
+# Route pour  recevoir la clé  du client
+@app.route('/secret_exchange' , methods =['POST'])
+def secret_exchange():
+	# recuperer les données envoyées par le client : 
+	data = flask.request.json
+	if 'secret_key_ciphered' in data :
+		cipher_secret= data['secret_key_ciphered']
+		# calcul de la cle partagé par le client
+		secret = pow(cipher_secret , d , n)
+		print(f'cle partage : {secret}')
+		return flask.jsonify({'secret_key': secret})
+
+	else:
+		return flask.jsonify({'Error': 'client cipher secret not found'})     
+
+# Route pour envoyer la cle pub
+@app.route("/pub_key_exchange" , methods =['GET'])
+def pub_key_exchange():
+	return flask.jsonify({'public_key':{'e':e , 'n':n}})
+
+
+if __name__ == "__main__":
+	app.run(host ='localhost' , port = 5001 ,debug = True)
